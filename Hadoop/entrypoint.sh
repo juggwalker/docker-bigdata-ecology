3602edd859b3a60fec5744ec5b44ea7484b0ea16
jugg.walker <jugg.walker@qq.com>
2019-04-29 18:16:48 +0800

    debug

--- Hadoop/entrypoint.sh
+++ Hadoop/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -eu
 
+is_empty_dir(){
+    return `ls -A $1|wc -w`
+}
+
+if [ ! -d $HADOOP_NAMENODE_DIR ] || is_empty_dir $HADOOP_NAMENODE_DIR; then
+    $HADOOP_HOME/bin/hdfs namenode -format
+fi
+
 /usr/sbin/sshd -D &
 
 exec "$@"
