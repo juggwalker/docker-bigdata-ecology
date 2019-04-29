21f0cc979c2856957ad414f81615205a12bc1c65
jugg.walker <jugg.walker@qq.com>
2019-04-29 12:42:17 +0800

    debug

--- Hadoop/start-container.sh
+++ Hadoop/start-container.sh
@@ -1,4 +1,4 @@
 
 
 
-docker run --rm --name hadoop-master -p 50070:50070 -p 8088:8088 -v /home/dockerdata/hadoop-data:/data hadoop:master
+docker run --rm --name hadoop-master --add-host hadoop-master:127.0.0.1 -p 50070:50070 -p 8088:8088 -v /home/dockerdata/hadoop-data:/data hadoop:master
