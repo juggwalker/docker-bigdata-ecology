87c8bff50244c9c7aed1664104bf7046e5bc1af6
tony <cheerfultony@gmail.com>
2019-05-07 19:09:06 +0800

    debug

--- Zookeeper/build.sh
+++ Zookeeper/build.sh
@@ -3,7 +3,7 @@
 echo ""
 
 echo -e "\nbuild zookeeper-master image. base zookeeper-3.4.14 \n"
-sudo docker build -t zookeeper:master .
+docker build -t zookeeper:master .
 
 
 echo ""
--- Zookeeper/start-container.sh
+++ Zookeeper/start-container.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 
 
+docker run -d --name zk-master --add-host hadoop-master:127.0.0.1 -p 2181:2181 -v //d/docker-bigdata-ecology/tmpdata/zk-data:/data zookeeper:master
 
-docker run -d --name zk-master --add-host hadoop-master:127.0.0.1 -p 2181:2181 -v /home/dockerdata/zk-data:/data zookeeper:master
